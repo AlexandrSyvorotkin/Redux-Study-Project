071d53f811d33ecf5c9a26dbb6074521621eaad3
Alex <Alex031994@gmail.com>
2022-03-24 23:47:55 +0300

    create remove and clear filters logic

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,11 +1,13 @@
 import {JobPosition} from './JobPosition';
 import {useDispatch, useSelector} from "react-redux";
-import {selectAllPositions} from "../store/positions/position-selectors";
+import {selectAllPositions, selectVisiblePostions} from "../store/positions/position-selectors";
 import {addFilter} from "../store/filters/filter-actions";
+import {selectFilters} from "../store/filters/filter-selector";
 
 const JobList = () => {
 
-    const positions = useSelector(selectAllPositions)
+    const currentFilters = useSelector(selectFilters)
+    const positions = useSelector(state => selectVisiblePostions(state, currentFilters))
     const dispatch = useDispatch()
 
     const handleAddFilter = (filter) => {
